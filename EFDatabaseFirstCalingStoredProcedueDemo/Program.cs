ba0429fdd4de3de4846d68b4f1808d25c9dadc88
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-05-25 12:35:45 +0530

    Insert Record

--- EFDatabaseFirstCalingStoredProcedueDemo/Program.cs
+++ EFDatabaseFirstCalingStoredProcedueDemo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,7 +27,24 @@ namespace EFDatabaseFirstCalingStoredProcedueDemo
 
             //*********************** Insert ***************
 
+              Employee employee = new Employee()
+            {
+                EmpName = "Sameer",
+                DeptID = 1,
+                CourseDuration = 40
 
+            };
+
+            SqlParameter[] parameters = new SqlParameter[]{
+                new SqlParameter("@EmpName",employee.EmpName),
+                new SqlParameter("@DeptID",employee.DeptID),
+                new SqlParameter("@CourseDuration",employee.CourseDuration)
+           };
+            //ADD EMPLOYEE
+            //var data2 = context.SPAddEmployees("Vishal", 1, 45); //error
+           var data2 = context.Database.ExecuteSqlCommand("SPAddEmployees @EmpName , @DeptID , @CourseDuration", parameters);
+            Console.WriteLine("Data inserted , rows changed =" + data2); //==> data2=1
+            context.SaveChanges();
 
             Console.Read();
         }
